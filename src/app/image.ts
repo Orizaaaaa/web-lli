5362be10905984b1c360f6716581420a27fbab04
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-04-04 16:49:30 +0700

    create page galery

--- src/app/image.ts
+++ src/app/image.ts
@@ -6,7 +6,10 @@ import image1 from '../assets/image1.svg'
 import image2 from '../assets/image2.svg'
 import image3 from '../assets/image3.svg'
 import newEvent from '../assets/newEvent.svg'
+import galeri1 from '../assets/galeri1 .svg'
+import galeri2 from '../assets/galeri2.svg'
+import galeri3 from '../assets/galeri3.svg'
 
 export {
-    logo1, logo2, home_hero1, news1, image1, image2, image3, newEvent
+    logo1, logo2, home_hero1, news1, image1, image2, image3, newEvent, galeri1, galeri2, galeri3
 }
--- src/app/organism/galery/index.tsx
+++ src/app/organism/galery/index.tsx
@@ -1,10 +1,32 @@
+import { galeri1, galeri2, galeri3 } from '@/app/image'
+import Image from 'next/image'
 import React from 'react'
 import { FaArrowRight } from 'react-icons/fa6'
 
 
 const Galery = () => {
+    const datalistGalery = [
+        {
+            image: galeri1
+        },
+        {
+            image: galeri2
+        },
+        {
+            image: galeri3
+        },
+        {
+            image: galeri1
+        },
+        {
+            image: galeri2
+        },
+        {
+            image: galeri3
+        },
+    ]
     return (
-        <section className="galery">
+        <section className="galery mb-24">
             <div className="container mx-auto">
                 <div className="hero-1 mb-4">
                     <div className="flex justify-between border-b-2 border-gray-300 py-5 mb-4">
@@ -13,6 +35,16 @@ const Galery = () => {
                             <h2 className='font-medium text-xl md:text-2xl' >All</h2>
                             <FaArrowRight size={20} />
                         </button>
+                    </div>
+                </div>
+                <div className="hero-2">
+                    <div className="list-galery">
+                        <div className="grid grid-cols-3 gap-3">
+                            {datalistGalery.map((item, index) => (
+                                <Image key={index} className='object-cover h-full w-full' src={item.image} alt='galery' />
+                            ))}
+
+                        </div>
                     </div>
                 </div>
             </div>
