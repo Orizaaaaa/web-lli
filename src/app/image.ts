5b06098a86eec781fa5bd08385edcf6271cfbad1
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-04-04 11:07:33 +0700

    finish page news

--- src/app/image.ts
+++ src/app/image.ts
@@ -2,6 +2,10 @@ import logo1 from '../assets/logo1.svg'
 import logo2 from '../assets/logo2.svg'
 import home_hero1 from '../assets/home_hero1.svg'
 import news1 from '../assets/news1.svg'
+import image1 from '../assets/image1.svg'
+import image2 from '../assets/image2.svg'
+import image3 from '../assets/image3.svg'
+
 export {
-    logo1, logo2, home_hero1, news1
+    logo1, logo2, home_hero1, news1, image1, image2, image3
 }
--- src/app/organism/news/index.tsx
+++ src/app/organism/news/index.tsx
@@ -1,13 +1,29 @@
 import ButtonPrimary from '@/app/components/elements/buttonPrimary';
-import { news1 } from '@/app/image';
+import { image1, image2, image3, news1 } from '@/app/image';
 import Image from 'next/image';
 import React from 'react'
 import { FaArrowRight } from "react-icons/fa6";
 
 const News = () => {
+    const datahero3 = [
+        {
+            image: image1,
+            title: 'Lorem Ipsum is simply dummy text of the.'
+        },
+        {
+            image: image2,
+            title: 'Lorem Ipsum is simply dummy text of the.'
+        },
+        {
+            image: image3,
+            title: 'Lorem Ipsum is simply dummy text of the.'
+        },
+    ]
     return (
         <section className='news mt-12 mb-12' id='news' >
             <div className="container mx-auto">
+
+                {/* hero1 */}
                 <div className="hero-1">
                     <div className="flex justify-between border-b-2 border-gray-300 py-5">
                         <h1 className='font-bold text-2xl' >Berita Terbaru</h1>
@@ -17,10 +33,12 @@ const News = () => {
                         </button>
                     </div>
                 </div>
+
+                {/* hero2 */}
                 <div className="hero-2 mt-6">
                     <div className="grid grid-cols-5 gap-10">
-                        <div className="col-span-3">
-                            <Image className='w-full h-full' src={news1} alt="news" />
+                        <div className="col-span-3 ">
+                            <Image className='w-full h-full object-cover' src={news1} alt="news" />
                         </div>
 
                         <div className="desc col-span-2 flex flex-col justify-between h-full">
@@ -28,15 +46,24 @@ const News = () => {
                                 <h1 className='text-2xl font-medium' >Lorem Ipsum is simply dummy text of the.</h1>
                                 <p className='mt-4' >Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                             </div>
-
-                            <ButtonPrimary className='rounded-md w-full' >
+                            <ButtonPrimary className='rounded-md w-full font-semibold' >
                                 Buka Berita
                             </ButtonPrimary>
-
                         </div>
                     </div>
+                </div>
+
+                {/* hero 3 bottom*/}
+                <div className="grid grid-cols-3 gap-3 mt-3">
+                    {datahero3.map((item, index) => (
+                        <div key={index}>
+                            <Image className='w-full h-full' src={item.image} alt='news_btm' />
+                            <p>{item.title}</p>
+                        </div>
+                    ))}
 
                 </div>
+
 
             </div>
         </section>
