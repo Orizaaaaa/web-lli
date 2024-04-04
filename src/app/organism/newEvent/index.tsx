a6cd0a06af71be4c6d70041f3c492c40fd100a6b
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-04-04 11:47:13 +0700

    finish responsive news and event

--- src/app/organism/newEvent/index.tsx
+++ src/app/organism/newEvent/index.tsx
@@ -7,11 +7,11 @@ const NewEvents = () => {
     return (
         <section className="new-events">
             <div className="container mx-auto">
-                <div className="hero-1">
+                <div className="hero-1 mb-4">
                     <div className="flex justify-between border-b-2 border-gray-300 py-5">
-                        <h1 className='font-bold text-2xl' >Acara Terbaru</h1>
+                        <h1 className='font-bold text-xl md:text-2xl' >Acara Terbaru</h1>
                         <button className="flex justify-center items-center gap-2">
-                            <h2 className='font-medium text-2xl' >All</h2>
+                            <h2 className='font-medium text-xl md:text-2xl' >All</h2>
                             <FaArrowRight size={20} />
                         </button>
                     </div>
--- src/app/organism/news/index.tsx
+++ src/app/organism/news/index.tsx
@@ -21,30 +21,30 @@ const News = () => {
     ]
     return (
         <section className='news mt-12 mb-12' id='news' >
-            <div className="container mx-auto">
+            <div className="container mx-auto p-2 lg:p-0">
 
                 {/* hero1 */}
                 <div className="hero-1">
                     <div className="flex justify-between border-b-2 border-gray-300 py-5">
-                        <h1 className='font-bold text-2xl' >Berita Terbaru</h1>
+                        <h1 className='font-bold text-xl md:text-2xl' >Berita Terbaru</h1>
                         <button className="flex justify-center items-center gap-2">
-                            <h2 className='font-medium text-2xl' >All</h2>
+                            <h2 className='font-medium text-xl md:text-2xl' >All</h2>
                             <FaArrowRight size={20} />
                         </button>
                     </div>
                 </div>
 
                 {/* hero2 */}
-                <div className="hero-2 mt-6">
-                    <div className="grid grid-cols-5 gap-10">
+                <div className="hero-2 mt-6 ">
+                    <div className="grid grid-cols-1 md:grid-cols-5 gap-3 lg:gap-10 ">
                         <div className="col-span-3 ">
                             <Image className='w-full h-full object-cover' src={news1} alt="news" />
                         </div>
 
                         <div className="desc col-span-2 flex flex-col justify-between h-full">
                             <div className="flex flex-col ">
-                                <h1 className='text-2xl font-medium' >Lorem Ipsum is simply dummy text of the.</h1>
-                                <p className='mt-4' >Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                                <h1 className='text-xl md:text-2xl font-medium' >Lorem Ipsum is simply dummy text of the.</h1>
+                                <p className='mt-4 text-sm md:text-base mb-3' >Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                             </div>
                             <ButtonPrimary className='rounded-md w-full font-semibold' >
                                 Buka Berita
@@ -54,14 +54,13 @@ const News = () => {
                 </div>
 
                 {/* hero 3 bottom*/}
-                <div className="grid grid-cols-3 gap-3 mt-3">
+                <div className="grid grid-cols-2 sm:grid-cols-3 gap-3 mt-3">
                     {datahero3.map((item, index) => (
                         <div key={index}>
-                            <Image className='w-full h-full' src={item.image} alt='news_btm' />
-                            <p>{item.title}</p>
+                            <Image className='w-full' src={item.image} alt='news_btm' />
+                            <p className='text-sm md:text-base' >{item.title}</p>
                         </div>
                     ))}
-
                 </div>
 
 
