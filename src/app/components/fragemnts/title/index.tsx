a7572472ad24de5f0b99fa49b3e992646fca5345
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-04-05 08:42:49 +0700

    update margin title

--- src/app/components/fragemnts/title/index.tsx
+++ src/app/components/fragemnts/title/index.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 const Title = ({ title, location }: Props) => {
     return (
-        <div className="flex justify-between border-b-2 border-gray-300 py-5">
+        <div className="flex justify-between border-b-2 border-gray-300 py-5 mx-3 md:mx-0">
             <h1 className='font-bold text-xl md:text-2xl' >{title}</h1>
             <Link href={location} className="flex justify-center items-center gap-2" >
                 <h2 className='font-medium text-xl md:text-2xl' >All</h2>
