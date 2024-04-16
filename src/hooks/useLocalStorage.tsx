0d20339a62626fda29cccebf3181449e6adbd26b
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-04-16 11:08:26 +0700

    update 3

--- src/hooks/useLocalStorage.tsx
+++ src/hooks/useLocalStorage.tsx
@@ -8,21 +8,20 @@ function useLocalStorage<T>(
   initialValue: T,
 ): [T, (value: SetValue<T>) => void] {
   // State to store our value
-  // Pass  initial state function to useState so logic is only executed once
+  // Pass initial state function to useState so logic is only executed once
   const [storedValue, setStoredValue] = useState(() => {
     try {
-      // Get from local storage by key
+      // Check if window is defined (only available in browser)
       if (typeof window !== "undefined") {
-        // browser code
         const item = window.localStorage.getItem(key);
         // Parse stored json or if none return initialValue
         return item ? JSON.parse(item) : initialValue;
       }
     } catch (error) {
-      // If error also return initialValue
+      // If error, return initialValue
       console.log(error);
-      return initialValue;
     }
+    return initialValue;
   });
 
   // useEffect to update local storage when the state changes
@@ -35,18 +34,14 @@ function useLocalStorage<T>(
           : storedValue;
       // Save state
       if (typeof window !== "undefined") {
-        // browser code
         window.localStorage.setItem(key, JSON.stringify(valueToStore));
       }
     } catch (error) {
-      // A more advanced implementation would handle the error case
       console.log(error);
     }
   }, [key, storedValue]);
 
   return [storedValue, setStoredValue];
-
-
 }
 
 export default useLocalStorage;
