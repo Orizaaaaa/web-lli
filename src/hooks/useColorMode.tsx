c20e2ae44e38666d8405fe13bfbc954f731e616d
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-04-16 11:01:43 +0700

    update1

--- src/hooks/useColorMode.tsx
+++ src/hooks/useColorMode.tsx
@@ -5,14 +5,16 @@ const useColorMode = () => {
   const [colorMode, setColorMode] = useLocalStorage("color-theme", "light");
 
   useEffect(() => {
-    //jika di classname terdapat dark akan menjadi warna hitam
-    //jika tidak akan menjadi warna putih karena dark nya di remove lewat className
-    const className = "dark";
-    const bodyClass = window.document.body.classList;
+    if (typeof window !== "undefined") {
+      //jika di classname terdapat dark akan menjadi warna hitam
+      //jika tidak akan menjadi warna putih karena dark nya di remove lewat className
+      const className = "dark";
+      const bodyClass = window.document.body.classList;
 
-    colorMode === "dark"
-      ? bodyClass.add(className)
-      : bodyClass.remove(className);
+      colorMode === "dark"
+        ? bodyClass.add(className)
+        : bodyClass.remove(className);
+    }
   }, [colorMode]);
 
   return [colorMode, setColorMode];
