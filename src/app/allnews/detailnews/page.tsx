052d034cdc621b6a2dc3334406d53c8e8d5e07a8
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-04-17 17:17:17 +0700

    finish author

--- src/app/allnews/detailnews/page.tsx
+++ src/app/allnews/detailnews/page.tsx
@@ -1,7 +1,9 @@
 import { human1, logo1, logo2 } from '@/app/image'
 import Image from 'next/image'
 import React from 'react'
-
+import { FaWhatsapp } from "react-icons/fa";
+import { RiFacebookBoxLine } from "react-icons/ri";
+import { MdOutlineEmail } from "react-icons/md";
 
 const DetailNews = () => {
     return (
@@ -17,7 +19,7 @@ const DetailNews = () => {
                     <h2 className='font-bold text-xl md:text-2xl text-justify mt-4 text-primaryred' >30 Maret 2024</h2>
                 </div>
 
-                <div className="author flex mt-3 border-y-2 border-[#929292] py-3 px-2">
+                <div className="author flex justify-between mt-3 border-y-2 border-[#929292] py-3 px-2">
                     <div className="profile flex gap-4">
                         <Image className='w-10 h-10' src={human1} alt='profile' />
                         <div className="name">
@@ -26,7 +28,14 @@ const DetailNews = () => {
                         </div>
                     </div>
 
+                    <div className="contact flex gap-4 justify-center items-center">
+                        <RiFacebookBoxLine size={31} />
+                        <FaWhatsapp size={31} />
+                        <MdOutlineEmail size={31} />
+                    </div>
                 </div>
+
+
             </section>
         </section>
     )
