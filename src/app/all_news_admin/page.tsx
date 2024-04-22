5387d04c0d357e506fec22d31fade10afe01a6cb
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-04-22 10:28:17 +0700

    responsive all article

--- src/app/all_news_admin/page.tsx
+++ src/app/all_news_admin/page.tsx
@@ -36,8 +36,6 @@ const AllNewsAdmin = () => {
 
 
 
-
-
     return (
         <DefaultLayout>
             <section className='news mb-12' id='news' >
@@ -48,25 +46,41 @@ const AllNewsAdmin = () => {
                     {/* hero 2 bottom*/}
                     <div className="grid grid-cols-2 sm:grid-cols-3 gap-3 mt-8">
                         {datahero3.map((item, index) => (
-
-                            <div className='shadow-6 p-2 rounded-md' key={index}>
-                                <Image className='w-full rounded-md' src={item.image} alt='news_btm' />
-                                <p className='text-sm md:text-base' >{item.title}</p>
-                                <button className='flex justify-end w-full' onClick={openModal}>
-                                    <BsThreeDots size={25} />
-                                </button>
-
-                                {/* modal */}
-                                <Modal isOpen={modalIsOpen} >
-                                    <p>kontol pantek</p>
-                                    <button className='bg-red' onClick={closeModal} >close</button>
-                                </Modal>
-                            </div>
+                            <>
+                                <div className='shadow-6 p-2 rounded-md' key={index}>
+                                    <Image className='w-full rounded-md' src={item.image} alt='news_btm' />
+                                    <p className='text-sm md:text-base' >{item.title}</p>
 
 
+
+                                    {/* <button className='flex justify-end w-full' onClick={openModal}>
+                                        <BsThreeDots size={25} />
+                                    </button> */}
+
+                                    <div className="button-action mt-3 grid gap-2 grid-cols-2 text-sm md:text-base md:grid-cols-3 justify-between">
+                                        <button className='update bg-black rounded-md py-1 px-2 text-white'>
+                                            Update
+                                        </button>
+
+                                        <button className='update bg-black rounded-md py-1 px-2 text-white'>
+                                            Detail
+                                        </button>
+                                        <button className='update bg-red rounded-md py-1 px-2 text-white'>
+                                            Delete
+                                        </button>
+                                    </div>
+
+                                    {/* modal */}
+                                    <Modal isOpen={modalIsOpen} >
+                                        <p>Isi dari modal</p>
+                                        <button className='bg-red' onClick={closeModal} >close</button>
+                                    </Modal>
+                                </div>
+                            </>
                         ))}
                     </div>
                 </div>
+
             </section>
         </DefaultLayout>
     )
